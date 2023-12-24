2b51bdea06e6fb9844896bd025a64fa3712adde4
Ilya Lomatsky <ilomatsky@gmail.com>
2023-12-25 01:38:23 +0600

    Added twirlers, described the interface of the form,

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,63 @@
+import ButtonSpinner from '../Spinner/ButtonSpinner';
+
 const Form = () => {
   return (
-    <div>
-      Form
-    </div>
+    <form onSubmit={onFormSubmit}>
+      <h4>{isEdit ? 'Edit contact' : 'Add new contact'}</h4>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          required
+          type="text"
+          name="name"
+          id="name"
+          className="form-control"
+          value={dish.name}
+          onChange={changeDish}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="phone">Phone</label>
+        <input
+          required
+          type="number"
+          name="phone"
+          id="phone"
+          className="form-control"
+          value={dish.description}
+          onChange={changeDish}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          required
+          type="email"
+          name="email"
+          id="email"
+          className="form-control"
+          value={dish.price}
+          onChange={changeDish}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="photo">Photo</label>
+        <input
+          required
+          type="url"
+          name="photo"
+          id="photo"
+          className="form-control"
+          value={dish.image}
+          onChange={changeDish}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-info mt-2" disabled={isLoading}>
+        {isLoading && <ButtonSpinner/>}
+        {isEdit ? 'Done' : 'Add'}
+      </button>
+    </form>
   );
 };
 
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export interface Contact {
+  id: string;
+  name: string;
+  phoneNumber: string;
+  email: string;
+  photoUrl: string;
+}
+
+export interface FormData extends Omit<Contact, 'id'> {}
